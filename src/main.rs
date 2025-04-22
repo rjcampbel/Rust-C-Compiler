ef6b5d433223160615510e4b55aeaf4f620844b6
Ryan Campbell <ryan.campbell4@gmail.com>
2025-04-22 16:58:31 -0500

    Add command line argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{builder::ArgPredicate, Args, Parser};
+use std::process::ExitCode;
+
+/// Simple program to greet a person
+#[derive(Parser,Debug)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// Source file to compile
+    file: String,
+
+    #[command(flatten)]
+    command: Command,
+}
+
+#[derive(Args,Debug)]
+#[group(required = false, multiple = false)]
+struct Command
+{
+    /// Run the the lexer, parser, and codegen
+    #[arg(id = "codegen", short, long, default_value_t = true, default_value_ifs([("lex", ArgPredicate::IsPresent, Some("false")), ("parse", ArgPredicate::IsPresent, Some("false"))]))]
+    run_codegen: bool,
+
+    /// Run the lexer and the parser
+    #[arg(id = "parse", short, long, default_value_t = true, default_value_if("lex", ArgPredicate::IsPresent, Some("false")))]
+    run_parser: bool,
+
+    /// Run the lexer
+    #[arg(id = "lex", short, long, default_value_t = true, conflicts_with_all(["parse", "codegen"]))]
+    run_lexer: bool,
+}
+
+fn main() -> ExitCode {
+    let args: Cli = Cli::parse();
+
+    println!("Args {:?}", args);
+
+    return 0.into();
 }
